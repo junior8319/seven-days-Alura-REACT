621a995953461e2d055dce229a43f06ac604936f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-04-23 15:35:23 -0300

    components: Home - add the NewsSubscription to Home.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import Menu from "./Menu";
+import NewsSubscription from "./NewsSubscription";
 
 function Home() {
   return (
     <div>
       <Menu />
       <h1>Home</h1>
+      <NewsSubscription />
     </div>
   );
 }
